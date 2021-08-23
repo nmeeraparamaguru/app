6940ebdb039c53eb2708cf3db83016e55e1a1c84
nmeeraparamaguru <nnandy593@gmail.com>
2021-08-23 17:08:37 +0530

    first commit

---
+++ TutorialApp/app/view/main/MainController.js
@@ -0,0 +1,18 @@
+Ext.define('TutorialApp.view.main.MainController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.main',
+
+    onClickButton: function () {
+
+        // Remove the localStorage key/value
+        localStorage.removeItem('TutorialLoggedIn');
+
+        // Remove Main View
+        this.getView().destroy();
+
+        // Add the Login Window
+        Ext.widget('login');
+
+    }
+});
+
